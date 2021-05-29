2186fbbcc2575129f587ca9bccbc3365f5aaf9b0
beifeng15 <974191367@qq.com>
2021-05-29 17:15:10 +0800

    feat:Orika工具类的增加

---
+++ src/test/java/com/wu/utils/User.java
@@ -0,0 +1,29 @@
+package com.wu.utils;
+
+/**
+ * @auther Mr.Wu
+ * @date 2021/5/29 17:07
+ */
+public class User {
+
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
